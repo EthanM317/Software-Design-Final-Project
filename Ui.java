22c64ac9ecc8a901c2bd0f1f23888a9a3da020b5
EthanM <ethan.medeiros1@ontariotechu.net>
2025-01-23 20:37:28 -0500

    finished addProduct and removeProduct messages

--- Ui.java
+++ Ui.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 
 public class Ui {
 
-    public void addProduct() {
+    public static void addProduct() {
+        //start scanner
         Scanner scanner = new Scanner(System.in);
-        String addMore = "Y";
-        while(scanner.hasNext() && !addMore.equals("N")){
+        //tracks if user wants to continue or not
+        String keepGoing = "";
+
+        //gets values of each product property
+        while(scanner.hasNext() && keepGoing.equals("Y")){
             System.out.println("Enter product name: "); 
             String productName = scanner.nextLine();
             System.out.println("Enter ID: "); 
@@ -14,17 +18,27 @@ public class Ui {
             System.out.println("Enter cost: "); 
             float cost = scanner.nextFloat();
 
+            //adds product
             Backend.addProduct(productName, cost, productID);
 
+            //asks if user wants to continue
             System.out.println("Do you want to add another product? Enter Y or N ");
-            addMore = scanner.nextLine(); 
+            keepGoing = scanner.nextLine();
+            //ensures user entered a valid value for keepGoing
+            while(!keepGoing.equals("Y") && !keepGoing.equals("N") && !keepGoing.equals("y") && !keepGoing.equals("n")) {
+                System.out.print("Please enter Y or N: ");
+                keepGoing = scanner.nextLine();
+            }
         }
     }
 
-    public void removeProduct() {
-        Scanner scanner = new Scanner(Scanner(System.in));
-        String remove = "Y"
-        while(scanner.hasNext() && !remove.equals("N")){
+    public static void removeProduct() {
+        //stats scanner
+        Scanner scanner = new Scanner(System.in);
+        //tracks if user wants to continue removing items
+        String keepGoing = "";
+        //gets each property of the object you want to remove
+        while(scanner.hasNext() && keepGoing.equals("Y")){
             System.out.println("Enter product name: "); 
             String productName = scanner.nextLine();
             System.out.println("Enter ID: "); 
@@ -32,10 +46,17 @@ public class Ui {
             System.out.println("Enter cost: "); 
             float cost = scanner.nextFloat();
 
+            //removes product
             Backend.removeProduct(productName, cost, productID);
 
+            //asks user if they want to remove another product
             System.out.println("Do you want to remove another product? Enter Y or N ");
-            remove = scanner.nextLine(); 
+            keepGoing = scanner.nextLine(); 
+            //ensures a valid value for keepGoing
+            while(!keepGoing.equals("Y") && !keepGoing.equals("N") && !keepGoing.equals("y") && !keepGoing.equals("n")) {
+                System.out.print("Please enter Y or N: ");
+                keepGoing = scanner.nextLine();
+            }
         }
     }
 }
