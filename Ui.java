a3e8e60d1782190969382e578c36be293e32258b
YogiPatelOTU <144565668+YogiPatelOTU@users.noreply.github.com>
2025-01-23 19:38:20 -0500

    Update Ui.java

--- Ui.java
+++ Ui.java
@@ -1,3 +1,13 @@
+import java.util.Scanner; 
+
+
 public class Ui {
-    
-}
+
+    public void addProduct() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What product do you want?"); 
+        while(scanner.hasNext()){
+            String productName = scanner.nextLine();
+            int productID = scanner.nextInt(); 
+            float cost = scanner.nextFloat();
+        }
