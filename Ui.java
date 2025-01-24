894c6f8e90fdbf3abf8b586aaab49e7391ecefec
brandonn14 <144816420+brandonn14@users.noreply.github.com>
2025-01-23 19:50:49 -0500

    Update Ui.java

--- Ui.java
+++ Ui.java
@@ -5,11 +5,20 @@ public class Ui {
 
     public void addProduct() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What product do you want?");
-        while (scanner.hasNext()) {
+        System.out.println("What product do you want?"); 
+        String addMore = "Y";
+        while(scanner.hasNext() && !addMore.equals("N")){
             String productName = scanner.nextLine();
-            int productID = scanner.nextInt();
+            int productID = scanner.nextInt(); 
             float cost = scanner.nextFloat();
-        }
+
+            Backend.addProduct(productName, cost, productID);
+
+            System.out.println("Do you want to add another product? Enter Y or N ");
+            addMore = scanner.nextLine();
+
+
+        } 
     }
 }
+
